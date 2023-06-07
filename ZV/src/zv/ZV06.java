0e346fc07e72a8f5fc2727fbcd69cbd2bc228f90
Márta Krisztián <martakrisztian97@gmail.com>
2023-06-07 13:47:51 +0200

    u

--- ZV/src/zv/ZV06.java
+++ ZV/src/zv/ZV06.java
@@ -5,7 +5,7 @@ public class ZV06 {
     // Írjon függvényt vagy metódust, amely kiszámolja az n-edik Fibonacci számot!
     // A Fibonacci sorozatot az an=an-2+an-1 rekurzióval definiálja (n>2), ahol a1=a2=1.
     public static int fibonaccsi(int n) {
-        if (n < 2) return 1;
+        if (n <= 2) return 1;
         else return fibonaccsi(n-2) + fibonaccsi(n-1);
     }
     
