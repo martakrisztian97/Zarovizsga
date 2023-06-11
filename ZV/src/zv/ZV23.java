7c2f952fd656c147d9978d7d24e3aa67ca7502d0
Márta Krisztián <martakrisztian97@gmail.com>
2023-06-11 15:28:19 +0200

    u

--- ZV/src/zv/ZV23.java
+++ ZV/src/zv/ZV23.java
@@ -5,7 +5,40 @@ public class ZV23 {
     // Adjon olyan metódust vagy függvényt, ami visszaadja, hogy a paramétereként megadott
     // (pozitív egészekből álló) nemüres tömbben melyik index az, ahol a leghosszabb
     // folyamatosan növekvő részsorozat kezdődik. Ha több ilyen index is van, az utolsót adja Vissza.
-    /*public static int resz(int[] szamok) {
-        int ;
-    }*/
+    public static int resz(int[] t) {
+        int hossz = 0;
+        int leg = 0;
+        int ind = 0;
+        int i;
+        for (i = 0; i < t.length-1; i++) {
+            if (t[i] < t[i+1]) {
+                hossz++;
+            } else {
+                if (leg <= hossz) {
+                    leg = hossz;
+                    ind = i - hossz;
+                }
+                hossz = 0;
+            }
+        }
+        
+        if (leg <= hossz) {
+            leg = hossz;
+            ind = i - hossz;
+        }
+        return ind;
+    }
+    
+    public static void main(String[] args) {
+        int[] szamok = {1, 2, 3};
+        int[] szamok2 = {1, 2, 3, 4, 1, 2, 3, 4};
+        int[] szamok3 = {1};
+        int[] szamok4 = {1, 1};
+        int[] szamok5 = {2, 1, 1};
+        System.out.println(resz(szamok));
+        System.out.println(resz(szamok2));
+        System.out.println(resz(szamok3));
+        System.out.println(resz(szamok4));
+        System.out.println(resz(szamok5));
+    }
 }
